606cac0326641b7c3802af7a2af19b8779b992c8
Adan Saavedra <adansaavedraa@icloud.com>
2022-10-04 17:24:06 -0500

    add Readme

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
 const h1 = document.querySelector('h1')
-const p = document.querySelector('p')
-const parrafito = document.querySelector('.parrafito')
-const pid = document.getElementById('pid')
-const input = document.querySelector('input')
+const form = document.querySelector('#form')
+const input1 = document.querySelector('#calculo1')
+const input2 = document.querySelector('#calculo2')
+const btn = document.querySelector('#btnCalcular')
+const pResult = document.querySelector('#result')
 
-console.log(input.value);
+form.addEventListener('submit',btnOnClick);
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
-
-h1.innerText = 'Patito <br> Feo';
-console.log(h1.getAttribute('class'));
-//h1.setAttribute('class', 'rojo')
-
-h1.classList.add('rojo')
-h1.classList.remove("verde")
-
-//h1.classList.toggle('verde');
-//h1.classList.contains('verde');
-
-input.value='456';
-
-const img= document.createElement('img');
-img.setAttribute('src','https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1');
-console.log(img);
-
-pid.innerHTML = '';
-pid.append(img);
+function btnOnClick(event){
+    event.preventDefault()
+    let result = (parseInt(input1.value) + parseInt(input2.value))
+    pResult.innerText = (`El Resultado es: ${result}`)
+}
